2002fb21490c017e29d49b9fdd3a960fdb24ecb9
harshitverma2103 <harshitverma2103@gmail.com>
2024-08-20 22:33:15 +0530

    introduce router and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Collection from "./pages/Collection";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
 
 const App = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/placrOrder" element={<PlaceOrder />} />
+        <Route path="product" element={<Product />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
