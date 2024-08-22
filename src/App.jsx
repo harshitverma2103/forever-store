55e86b08a7d2190335b4334aee65dd04788a9d66
harshitverma2103 <harshitverma2103@gmail.com>
2024-08-22 12:48:02 +0530

    add sidebar for mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import Login from "./pages/Login";
 import Orders from "./pages/Orders";
 import PlaceOrder from "./pages/PlaceOrder";
 import Product from "./pages/Product";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
